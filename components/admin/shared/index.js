541899dfc7b977254373455dd79e7f8006fd2141
soufians123 <soufiansoufiane19900@gmail.com>
2025-09-15 21:38:58 +0100

    صلاح مشكلة import في shared/index.js

--- components/admin/shared/index.js
+++ components/admin/shared/index.js
@@ -1,5 +1,4 @@
 export { default as AdminTable } from './AdminTable';
 export { default as StatsCards } from './StatsCards';
-export { default as SearchAndFilter } from './SearchAndFilter';
 export { default as AdminModal } from './AdminModal';
 export { default as ActionButtons } from './ActionButtons';
